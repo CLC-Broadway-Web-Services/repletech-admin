366b09a9654a6b2f44644c94b522d0e79f8f0fb3
Ahtesham <ahtesham2000@gmail.com>
2021-09-18 21:15:03 +0530

    first commit

---
+++ config/database.js
@@ -0,0 +1,20 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        host: env('DATABASE_HOST', 'testcluster.khj5r.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'Repletech'),
+        username: env('DATABASE_USERNAME', 'ahtesham2000'),
+        password: env('DATABASE_PASSWORD', 'ahtesham2000'),
+      },
+      options: {
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
+      },
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e2e38aee-2ad7-41a9-b245-05fac99962de'
+};
